59e2038c27349610f88e95bc27ccd046f3624c4c
100s4u <sandman.4ever@mail.ru>
2021-10-24 15:01:48 +0300

    hash

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Add } from './components/Add';
 import { Edit } from './components/Edit';
 import { Reg } from './components/Reg';
 import { LogIn } from './components/LogIn';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
           <Route exact path="/add" component={ Add } />
           <Route exact path="/edit/:id" component={ Edit } />
+        <Switch>
+          <Route exact path="/" component={ Home } />
           <Route exact path="/login" component={ LogIn } />
           <Route exact path="/reg" component={ Reg } />
-          <Route exact path="/" component={ Home } />
         </Switch>
       </Router>
     </div>
